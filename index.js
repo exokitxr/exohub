7abba6b10744ebc4abc7f09cb1d6f926126703d0
Adrian Biedrzycki <a@modules.io>
2018-04-18 03:53:23 +0000

    Add links serving support

--- index.js
+++ index.js
@@ -1,28 +1,42 @@
+const http = require('http');
 const https = require('https');
+const express = require('express');
 
 // const URL = `https://www.reddit.com/r/exokit.json`;
 
 const _parseLinks = j => j.data.children.map(child => child.data.url);
+let links = null;
 
-const req = https.get({
-  hostname: 'www.reddit.com',
-  path: '/r/exokit.json',
-  encoding: 'utf8',
-}, res => {
-  const bs = [];
-  res.on('data', d => {
-    bs.push(d);
+const _fetchLinks = () => {
+  const req = https.get({
+    hostname: 'www.reddit.com',
+    path: '/r/exokit.json',
+    encoding: 'utf8',
+  }, res => {
+    const bs = [];
+    res.on('data', d => {
+      bs.push(d);
+    });
+    res.on('end', () => {
+      const b = Buffer.concat(bs);
+      const s = b.toString('utf8');
+      const j = JSON.parse(s);
+      links = _parseLinks(j);
+    });
+    res.on('error', err => {
+      throw err;
+    });
   });
-  res.on('end', () => {
-    const b = Buffer.concat(bs);
-    const s = b.toString('utf8');
-    const j = JSON.parse(s);
-    console.log(_parseLinks(j));
-  });
-  res.on('error', err => {
+  req.on('error', err => {
     throw err;
   });
+};
+_fetchLinks();
+setInterval(_fetchLinks, 10 * 1000);
+
+const app = express();
+app.get('*', (req, res, next) => {
+  res.json(links);
 });
-req.on('error', err => {
-  throw err;
-});
+http.createServer(app)
+  .listen(9000);
